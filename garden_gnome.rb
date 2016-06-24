6c456990a650dcebd98ebb04a7b1b93ed391a175
Alex Appel <alexappel93@gmail.com>
2016-06-24 17:24:33 +0000

    team: @alexappel @julietstern

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,24 @@
-# Code your instances here
+class GardenGnome
+  attr_accessor :name, :age, :gluten_allergy, :hat_color
+  attr_reader :personality
+  def initialize(color = "red")
+    # @name
+    # @age
+    # @gluten_allergy
+    @personality = "evil"
+    @hat_color = color
+  end
+
+  def gnaw
+    return "Gnawing on a tree!!!"
+  end
+
+  def shout
+    return "GNARLY!!!"
+  end
+
+  def introduce_self
+    return "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
+  end
+
+end
